b7e6bb1250e42246fcd8d5050e1b35e624080ada
Guilherme Rosa <guilhermemuralha2016@gmail.com>
2026-04-05 10:30:49 -0300

    Integrando serviço de update de perfil

--- client/src/components/meProfile/index.tsx
+++ client/src/components/meProfile/index.tsx
@@ -3,11 +3,30 @@ import { FaRegEdit } from "react-icons/fa";
 import type { IUser } from "../../types/IUser";
 import { useImageUpload } from "./functions";
 import * as Style from "./MeProfileStyled";
+import { useUpdateMeMutation } from "../../services/Auth.Service";
 
 export const MeProfile = ({ cover, name, username, profile_image, following }: Omit<IUser, "email" | "password" | "birth_date">) => {
+  const [updateMe] = useUpdateMeMutation();
   const { images, onchangeFile, clear, isDisabled } = useImageUpload();
   const avatarSrc = images.avatar.preview || profile_image || "/avatar_default.jpg";
   const coverSrc = images.cover.preview || cover || "/banner.jpg";
+
+  const updateProfile = async () => {
+    const formData = new FormData();
+    if (images.cover.file) { formData.append("cover", images.cover.file); };
+    if (images.avatar.file) { formData.append("profile_image", images.avatar.file); };
+
+    try {
+      const res = await updateMe(formData).unwrap();
+      console.log(res);
+      alert("Atualização feita com sucesso!");
+      clear();
+
+    } catch (error) {
+      console.error(error);
+      alert("Ocorreu um erro ao realizar a atualização!");
+    }
+  };
 
   //Evitar memory leak
   useEffect(() => {
@@ -43,7 +62,8 @@ export const MeProfile = ({ cover, name, username, profile_image, following }: O
       </Style.ProfileBackground>
 
       <Style.Buttons>
-        <Style.ButtonEdit isDisabled={isDisabled} type="salvar">Salvar</Style.ButtonEdit>
+        <Style.ButtonEdit isDisabled={isDisabled} type="salvar"
+          onClick={updateProfile}>Salvar</Style.ButtonEdit>
         <Style.ButtonEdit isDisabled={isDisabled} type="cancelar"
           onClick={clear}>Cancelar</Style.ButtonEdit>
       </Style.Buttons>
