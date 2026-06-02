52a2c523ba883547155315e9a2708a5470fe4d16
Guilherme Rosa da Silva <guilhermemuralha2016@gmail.com>
2026-06-02 12:29:34 -0300

    :ok_hand: refactor: Refatorando a tela de Meu Perfil

--- client/src/containers/profiles/meProfile/index.tsx
+++ client/src/containers/profiles/meProfile/index.tsx
@@ -1,35 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { FaRegEdit } from "react-icons/fa";
 import { useUpdateMeMutation } from "../../../services/Users.Service";
 import { useImageUpload } from "./functions";
 import { FloatingInput } from "../../../components/input";
 import * as Style from "../MeProfileStyled";
 
-export const MeProfile = ({ cover, name, profile_image }:
-  Omit<IUser, "email" | "birth_date" | "following" | "password">) => {
+type MeProfileProps = Omit<IUser, "email" | "birth_date" | "following" | "password">;
+
+export const MeProfile = ({ cover, name, profile_image }: MeProfileProps) => {
   const [updateMe] = useUpdateMeMutation();
   const { images, onchangeFile, clear } = useImageUpload();
   const [tempName, setTempName] = useState<string>(name);
+  const [tempPassword, setTempPassword] = useState<string>('');
+  const [isSaving, setIsSaving] = useState(false);
+
   const avatarSrc = images.avatar.preview || profile_image || "/avatar_default.jpg";
   const coverSrc = images.cover.preview || cover || "/banner.jpg";
 
-  const updateProfile = async () => {
+  const updateProfile = useCallback(async () => {
+    setIsSaving(true);
     const formData = new FormData();
-    if (images.cover.file) { formData.append("cover", images.cover.file); };
-    if (images.avatar.file) { formData.append("profile_image", images.avatar.file); };
+    if (images.cover.file) formData.append("cover", images.cover.file);
+    if (images.avatar.file) formData.append("profile_image", images.avatar.file);
+    if (tempPassword) formData.append("password", tempPassword);
     formData.append("name", tempName);
 
     try {
-      const res = await updateMe(formData).unwrap();
+      const res = await updateMe(formData as Partial<IUser>).unwrap();
       console.log(res);
       alert("Atualização feita com sucesso!");
       clear();
-
     } catch (error) {
       console.error(error);
       alert("Ocorreu um erro ao realizar a atualização!");
+    } finally {
+      setIsSaving(false);
     }
-  };
+  }, [images, tempName, tempPassword, updateMe, clear]);
 
   //Evitar memory leak
   useEffect(() => {
@@ -43,39 +50,34 @@ export const MeProfile = ({ cover, name, profile_image }:
   return (
     <Style.ProfileContainer>
       <Style.ProfileBackground isCover={coverSrc}>
-
         <Style.EditCover>
           <label htmlFor="cover">
-            <FaRegEdit size={25} cursor='pointer' title="Editar Background" />
+            <FaRegEdit size={25} cursor="pointer" title="Editar Background" />
           </label>
-          <Style.InputNone id='cover' type="file" accept="image/*"
-            onChange={onchangeFile("cover")} />
+          <Style.InputNone id="cover" type="file" accept="image/*" onChange={onchangeFile("cover")} />
         </Style.EditCover>
 
         <Style.ProfileHeader>
           <Style.Avatar src={avatarSrc} alt="Foto de Perfil" />
           <Style.EditAvatar>
             <label htmlFor="profile">
-              <FaRegEdit size={45} cursor='pointer' title="Editar Avatar" />
+              <FaRegEdit size={45} cursor="pointer" title="Editar Avatar" />
             </label>
-            <Style.InputNone id="profile" type="file" accept="image/*"
-              onChange={onchangeFile("avatar")} />
+            <Style.InputNone id="profile" type="file" accept="image/*" onChange={onchangeFile("avatar")} />
           </Style.EditAvatar>
         </Style.ProfileHeader>
       </Style.ProfileBackground>
 
       <Style.ProfileInfo>
-        <FloatingInput label="Nome" id="name" type="text" value={tempName}
-          onChange={(e) => setTempName(e.target.value)} />
-        <FloatingInput label="Alterar Senha" id="password" type="password" />
+        <FloatingInput label="Nome" id="name" type="text" value={tempName} onChange={(e) => setTempName(e.target.value)} />
+        <FloatingInput label="Alterar Senha" id="password" type="password" onChange={(e) => setTempPassword(e.target.value)} />
       </Style.ProfileInfo>
 
-
-        <Style.Buttons>
-          <Style.ButtonEdit isDisabled={false} type="salvar"
-            onClick={updateProfile}>Salvar</Style.ButtonEdit>
-        </Style.Buttons>
-
+      <Style.Buttons>
+        <Style.ButtonEdit isDisabled={isSaving} type="salvar" onClick={updateProfile}>
+          {isSaving ? "Salvando..." : "Salvar"}
+        </Style.ButtonEdit>
+      </Style.Buttons>
     </Style.ProfileContainer>
-  )
+  );
 };
